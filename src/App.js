3981bd8ce1ec1a1bd5f3080e2a6e9782a012743c
Nchang victory <97144931+vickyXuzhou@users.noreply.github.com>
2023-04-05 23:38:28 +0800

    final touch

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   
   const [tabs,setTabs] = useState([])
   const [load,setLoad]= useState(true)
+  const [value,setValue] = useState(0)
   
   async function LoadItem(){
         setLoad(true)
@@ -31,7 +32,7 @@ function App() {
   LoadItem()
   // console.log(tabs)
   },[])
-  console.log(tabs)
+  // console.log(tabs)
 
   if(load){
     return(
@@ -41,16 +42,56 @@ function App() {
     )
   }
 
-
-  return <div>{
-    tabs.map(item=>{
+const {title,company,dates,duties} = tabs[value]
+  
+   
       return(
-        <section>
+        <section className="section ">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+      <div className="btn-container">
+        {/* btn container */
+        
+        tabs.map((item,index)=>{
+          return(
+            <button key={item.id} 
+            onClick={()=>setValue(index) }
+            className={`job-btn ${index === value && 'active-btn'}`} 
+          >{item.company }</button>
+          )
+        })
+        
+        }
+        </div>
+       
+        {/* job info */
+        duties.map(item=>{
+          return(
+            <div>
+              {item}
+            </div>
+          )
+        })
+        }
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
           
-        </section>
+        </article>
+      </div>
+      <button type="button" className="btn">
+        more info
+      </button>
+    </section>
       )
-    })
-    }</div>
-}
+      }
+
+
+  
+
 
 export default App
